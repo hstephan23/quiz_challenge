508c802c70ac10cd89e49e97ec7e928439ef287f
Harrison Stephan <harrisonstephan@Harrisons-iMac.local>
2023-12-19 12:18:22 -0700

    added the html and css and some javascript for the initials submit button

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//timer start amount
+ //timer start amount
 let time = 100;
 let questionCount = 1;
 let score = 0;
@@ -6,7 +6,8 @@ let score = 0;
 // various variables for elements or classes to be selected
 const startButton = document.getElementById("start");
 const answers = document.getElementsByClassName("item");
-const goBack = document.getElementById("goBack")
+const goBack = document.getElementById("goBack");
+const submit = document.getElementById("submit");
 
 // various listener functions
 if (startButton !== null) {
@@ -16,13 +17,14 @@ if (startButton !== null) {
         startGame();
         questionAppear(questionCount);
     });
-}
+};
 
 if (goBack !== null) {
-    goBack.addEventListener("click", function() {
-       window.location.href = "../index.html";
+    goBack.addEventListener("click", function(event) {
+        event.preventDefault();
+        window.location.href = "../index.html";
     });
-}
+};
 
 //iterating over the array of classes
 if (answers !== null) {
@@ -47,29 +49,46 @@ if (answers !== null) {
             }
         })
     }
-}
+};
+
+if (answers !== null) {
+    submit.addEventListener("click", function (event) {
+        event.preventDefault();
+        addToScoreBoard();
+        displayScoreBoard();
+    })
+};
 
 //functions
 function menuDisappear(id) {
     let erase = document.getElementById(id);
     erase.style.display = "none";
-}
+};
 
 function questionAppear(id) {
     let show = document.getElementById(id);
     show.style.display = "inline";
-}
+};
 
 function questionDisappear(id) {
     let disappear = document.getElementById(id);
     disappear.style.display = "none";
-}
+};
 
 function startGame() {
     time = 100;
-}
+};
 
 function inputInitials(finalID) {
     let input = document.getElementById(finalID);
     input.style.display = "inline";
+};
+
+//arrow functions
+const addToScoreBoard = () => {
+    
+};
+
+const displayScoreBoard = () => {
+    window.location.href ="./assets/scores.html";
 }
